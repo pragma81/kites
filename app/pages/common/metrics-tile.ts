f474af184d457faa98a9c919e4f12a31668778b2
Davide Antelmo <davide.antelmo@gmail.com>
2016-07-30 20:39:43 +0200

    save iteration

---
+++ app/pages/common/metrics-tile.ts
@@ -0,0 +1,35 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'da-metrics-tile',
+  template: `
+    <a class="tile-metric" (click)="onLegendClick($event)">
+                <div class="tile-metric-inner">
+                    <div class="tile-metric-image">
+                    </div>
+                    <div class="tile-metric-content">
+                        <span class="tile-metric-data">
+              {{value}}
+            </span>
+                        <div class="tile-metric-data-legend">
+                            {{legend}}
+                        </div>
+                    </div>
+                </div>
+            </a>
+  `,
+  styleUrls: ['app/common/metrics-tile.css']
+})
+export class MetricsTile {
+@Input() value = 0;
+@Input() legend = "";
+@Output() legendClickEmitter = new EventEmitter();
+
+onLegendClick(event ){
+  this.legendClickEmitter.emit({
+      value: this.value,
+      legend: this.legend
+    })
+}
+
+}
---
+++ app/pages/test-suite-explorer/test-suite-explorer.ts
@@ -0,0 +1,58 @@
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {Modal, NavController, } from 'ionic-angular'
+import {FeatureSearchModal} from './FeatureSearchModal'
+import {MetricsTile} from '../common/metrics-tile'
+import * as fs from 'fs'
+import * as hljs from 'highlight.js'
+
+declare var Gherkin : any ;
+
+@Component({
+  templateUrl: 'build/pages/dashboard/DashboardPage.html'
+})
+export class DashboardPage {
+  
+  public fileText: string = "";
+  public gherkinhljs: string;
+  @ViewChild('hljsarea') textRef: ElementRef;
+  constructor(private nav: NavController) {
+    let options: hljs.IOptions = { languages: ['gherkin'] };
+    hljs.configure(options);
+  }
+
+  showSearchModal() {
+    let modal = Modal.create(FeatureSearchModal);
+    modal.onDismiss(file => {
+      console.log(file.name);
+      console.log(file.path);
+      console.log(window['ipc']);
+      const fs = window['fs'];
+      let ref = this;
+      fs.readFile(file.path, 'utf-8', function (err, data) {
+        if (err) {
+          alert("An error ocurred reading the file :" + err.message);
+          return;
+        }
+        ref.fileText = data;
+      });
+      console.log(ref.fileText);
+      let result: hljs.IHighlightResult = hljs.highlight('gherkin', ref.fileText, true);
+      ref.gherkinhljs = result.value;
+
+      this.parse(ref.fileText);
+
+    });
+
+    this.nav.present(modal);
+  }
+
+  parse(featureFileText) {
+   
+    var parser = new Gherkin.Parser();
+    parser.stopAtFirstError = false;
+        var ast = parser.parse(featureFileText);
+        var result = JSON.stringify(ast, null, 2);
+      console.log(result);
+      
+  }
+}
